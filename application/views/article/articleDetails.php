545f072d019b7dce2af18032ec9f518746a69b8c
gtmalar71596 <gtmalar71596@gmail.com>
2017-02-08 11:13:31 +0800

    adding dynamic articals views

--- application/views/article/articleDetails.php
+++ application/views/article/articleDetails.php
@@ -8,8 +8,8 @@
                     <div class="action">
                         <div class="col-sm-12">
                             <h1 class="title">Article Details</h1>
-                           <!-- <p>Blog with right sidebar</p>-->
-                        </div>                                                                                
+                           <!-- <p>Blog with right sidebar</p>-->							     
+		                  </div>                                                                                
                     </div>
                 </div>
             </div>
@@ -31,15 +31,14 @@
                                     </div>
                                 </div>
                                 <div class="post-content overflow">
-                                    <h2 class="post-title bold"><a href="#">Advanced business cards design</a></h2>
-                                    <h3 class="post-author"><a href="#">Posted by micron News</a></h3>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. <br> <br>
-
-                                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+                                    <h2 class="post-title bold"><a href="#"><?php echo $article_detail[0]->article; ?></a></h2>
+                                    <h3 class="post-author"><a href="#">Posted by &nbsp;<?php echo $article_detail[0]->participant_name; ?></a></h3>
+                                    <p><?php echo $article_detail[0]->article; ?>                 
+									</p>
                                     <div class="post-bottom overflow">
                                         <ul class="nav navbar-nav post-nav">                                          
-                                            <li><a href="#"><i class="fa fa-heart"></i>32 Love</a></li>
-                                            <li><a href="#"><i class="fa fa-comments"></i>3 Comments</a></li>
+                                            <li><a href="#"><i class="fa fa-heart"></i><?php echo $article_fav_counts; ?> Love</a></li>
+                                            <li><a href="#"><i class="fa fa-comments"></i><?php echo $article_comment_counts; ?>&nbsp;Comments</a></li>
                                         </ul>
                                     </div>
                                     <div class="blog-share">
@@ -52,53 +51,27 @@
                                     <div class="response-area">
                                     <h2 class="bold">Comments</h2>
                                     <ul class="media-list">
+									<?php for ($i = 0; $i < count($article_comments); ++$i) { ?>
                                         <li class="media">
-                                            <div class="post-comment">
-                                                <a class="pull-left" href="#">
-                                                   <img class="media-object" src="<?php echo base_url(); ?>assets/images/blogdetails/3.png" alt="">
-                                                </a>
+                                            <div class="post-comment">                                               
                                                 <div class="media-body">
-                                                    <span><i class="fa fa-user"></i>Posted by <a href="#">Endure</a></span>
-                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+                                                    <span><i class="fa fa-user"></i>Posted by <a href="#"><?php echo $article_comments[$i]->user_id; ?></a></span>
+                                                    <p><?php echo $article_comments[$i]->comments; ?></p>
                                                     <ul class="nav navbar-nav post-nav">
-                                                        <li><a href="#"><i class="fa fa-clock-o"></i>February 11,2014</a></li>
-                                                        <li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>
+													<?php 
+													 $Cday = substr($article_comments[$i]->time_created,8, 2);
+													 $Cmonth = substr($article_comments[$i]->time_created,5, 2);
+													 $Cyear = substr($article_comments[$i]->time_created, 0,4);
+													 $CdateObj = DateTime::createFromFormat('!m', $Cmonth);
+													 $CmonthName = $CdateObj->format('F');
+													 ?>		
+                                                        <li><a href="#"><i class="fa fa-clock-o"></i><?php echo $CmonthName;?>&nbsp;<?php echo $Cday; ?> , <?php  echo $Cyear; ?></a></li>
+                                                        <!--<li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>-->
                                                     </ul>
                                                 </div>
-                                            </div>
-                                            <div class="parrent">
-                                                <ul class="media-list">
-                                                    <li class="post-comment reply">
-                                                        <a class="pull-left" href="#">
-                                                            <img class="media-object" src="<?php echo base_url(); ?>assets/images/blogdetails/3.png" alt="">
-                                                        </a>
-                                                        <div class="media-body">
-                                                            <span><i class="fa fa-user"></i>Posted by <a href="#">Endure</a></span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
-                                                            <ul class="nav navbar-nav post-nav">
-                                                                <li><a href="#"><i class="fa fa-clock-o"></i>February 11,2014</a></li>
-                                                            </ul>
-                                                        </div>
-                                                    </li>
-                                                </ul>
-                                            </div>
+                                            </div>                                         
                                         </li>
-                                        <li class="media">
-                                            <div class="post-comment">
-                                                <a class="pull-left" href="#">
-                                                    <img class="media-object" src="<?php echo base_url(); ?>assets/images/blogdetails/4.png" alt="">
-                                                </a>
-                                                <div class="media-body">
-                                                    <span><i class="fa fa-user"></i>Posted by <a href="#">Endure</a></span>
-                                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
-                                                    <ul class="nav navbar-nav post-nav">
-                                                        <li><a href="#"><i class="fa fa-clock-o"></i>February 11,2014</a></li>
-                                                        <li><a href="#"><i class="fa fa-reply"></i>Reply</a></li>
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        
+									<?php } ?>
                                     </ul>                   
                                 </div><!--/Response-area-->
                                 </div>
@@ -109,35 +82,29 @@
                 <div class="col-md-3 col-sm-5">
                     <div class="sidebar blog-sidebar">
                         <div class="sidebar-item  recent">
-                            <h3>Comments</h3>
-                            <div class="media">
-                                <div class="pull-left">
+                         <?php  if ($article_comment_counts > 0){ ?>
+                          <div class="sidebar-item  recent">						    
+                             <h3><?php echo 'Comments'; ?></h3>							 	
+							 <?php for ($i = 0; $i < count($article_comments); ++$i) { ?>
+                            <div class="media">    
+								<div class="pull-left">
                                     <a href="#"><img src="<?php echo base_url(); ?>assets/images/portfolio/project1.jpg" alt=""></a>
-                                </div>
+                                </div>                            
                                 <div class="media-body">
-                                    <h4><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit,</a></h4>
-                                    <p>posted on  07 March 2014</p>
-                                </div>
+                                    <h4><a href="#"><?php echo $article_comments[$i]->comments; ?></a></h4>
+									<?php 
+									  $CSday = substr($article_comments[$i]->time_created,8, 2);
+									  $CSmonth = substr($article_comments[$i]->time_created,5, 2);
+									  $CSyear = substr($article_comments[$i]->time_created, 0,4);
+									  $CSdateObj = DateTime::createFromFormat('!m', $Cmonth);
+									  $CSmonthName = $CdateObj->format('F');													 
+									 ?>	
+                                     <p>posted on  <?php echo $CSday; ?>&nbsp;<?php echo $CSmonthName;?>&nbsp;<?php  echo $CSyear;?></p>
+								</div>
                             </div>
-                            <div class="media">
-                                <div class="pull-left">
-                                    <a href="#"><img src="<?php echo base_url(); ?>assets/images/portfolio/project2.jpg" alt=""></a>
-                                </div>
-                                <div class="media-body">
-                                    <h4><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit,</a></h4>
-                                    <p>posted on  07 March 2014</p>
-                                </div>
-                            </div>
-                            <div class="media">
-                                <div class="pull-left">
-                                    <a href="#"><img src="<?php echo base_url(); ?>assets/images/portfolio/project3.jpg" alt=""></a>
-                                </div>
-                                <div class="media-body">
-                                    <h4><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit,</a></h4>
-                                    <p>posted on  07 March 2014</p>
-                                </div>
-                            </div>
+							 <?php }?>                           
                         </div> 
+						<?php } ?>
                         <div class="sidebar-item popular">
                             <h3>Latest Photos</h3>
                             <ul class="gallery">
@@ -155,84 +122,6 @@
         </div>
     </section>
     <!--/#blog-->
-
-    <footer id="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12 text-center bottom-separator">
-                    <img src="images/home/under.png" class="img-responsive inline" alt="">
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="testimonial bottom">
-                        <h2>Testimonial</h2>
-                        <div class="media">
-                            <div class="pull-left">
-                                <a href="#"><img src="images/home/profile1.png" alt=""></a>
-                            </div>
-                            <div class="media-body">
-                                <blockquote>Nisi commodo bresaola, leberkas venison eiusmod bacon occaecat labore tail.</blockquote>
-                                <h3><a href="#">- Jhon Kalis</a></h3>
-                            </div>
-                         </div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <a href="#"><img src="images/home/profile2.png" alt=""></a>
-                            </div>
-                            <div class="media-body">
-                                <blockquote>Capicola nisi flank sed minim sunt aliqua rump pancetta leberkas venison eiusmod.</blockquote>
-                                <h3><a href="">- Abraham Josef</a></h3>
-                            </div>
-                        </div>   
-                    </div> 
-                </div>
-                <div class="col-md-3 col-sm-6">
-                    <div class="contact-info bottom">
-                        <h2>Contacts</h2>
-                        <address>
-                        E-mail: <a href="mailto:someone@example.com">email@email.com</a> <br> 
-                        Phone: +1 (123) 456 7890 <br> 
-                        Fax: +1 (123) 456 7891 <br> 
-                        </address>
-
-                        <h2>Address</h2>
-                        <address>
-                        Unit C2, St.Vincent's Trading Est., <br> 
-                        Feeder Road, <br> 
-                        Bristol, BS2 0UY <br> 
-                        United Kingdom <br> 
-                        </address>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12">
-                    <div class="contact-form bottom">
-                        <h2>Send a message</h2>
-                        <form id="main-contact-form" name="contact-form" method="post" action="sendemail.php">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" required="required" placeholder="Name">
-                            </div>
-                            <div class="form-group">
-                                <input type="email" name="email" class="form-control" required="required" placeholder="Email Id">
-                            </div>
-                            <div class="form-group">
-                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Your text here"></textarea>
-                            </div>                        
-                            <div class="form-group">
-                                <input type="submit" name="submit" class="btn btn-submit" value="Submit">
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-sm-12">
-                    <div class="copyright-text text-center">
-                        <p>&copy; Your Company 2014. All Rights Reserved.</p>
-                        <p>Designed by <a target="_blank" href="http://www.themeum.com">Themeum</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!--/#footer-->
-
 
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
