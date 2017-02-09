3e5f4910f462f392041a582ce1aca13b9f042e54
gtmalar71596 <gtmalar71596@gmail.com>
2017-02-10 07:22:28 +0800

    adding community connect singnature

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -68,8 +68,8 @@
                 </div>
                 <div class="col-sm-12">
                     <div class="copyright-text text-center">
-                        <p>&copy; Your Company 2014. All Rights Reserved.</p>
-                        <p>Designed by <a target="_blank" href="http://www.themeum.com">Themeum</a></p>
+                       <p>&copy; Community Connect 2017. All Rights Reserved.</p>
+                        <p>Designed by <a target="_blank" href="http://www.themeum.com">Community Connect</a></p>
                     </div>
                 </div>
             </div>
