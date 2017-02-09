b1c5cb1d9ab376107afef7267fafbf3f9f20f491
gtmalar71596 <gtmalar71596@gmail.com>
2017-02-10 07:11:31 +0800

    svn commit

--- application/controllers/booking.php
+++ application/controllers/booking.php
@@ -5,26 +5,45 @@ class booking extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->library('session');
 		$this->load->helper('form');
 		$this->load->helper('url');
 		$this->load->database();
-		$this->load->library('form_validation');
 		$this->load->model('Bookingmodel');
 	}
 
-	function post_booking($event_id)
+	function post_booking($event_id, $user_email)
 	{   
-	    $this->load->model('Bookingmodel');
-		//$data['event'] = $this->Bookingmodel->get_event($event_id);
-		$data = array (
-				'event_id' => $this->input->post($event_id),
-				'booking_date' => $this->input->post(date("Y-m-d")),
-				'user_email' => $this->input->post('gmail.com')
+		$this->load->model('Bookingmodel');
+		$result = $this->Bookingmodel->getIsExistBooking($event_id, $user_email);
+		if ($result == 0)
+		{
+			$bookingdata = array (
+				'event_id' => $event_id,
+				'booking_date' => date('Y-m-d'),
+				'user_email' => $user_email
 				);				
-		$this->db->insert('tbl_booking', $data);
-		$this->session->set_flashdata('msg', '<div class="alert alert-success textcenter">Successfully joined event!</div>');
-		//redirect('events/getEvents');
+			$this->db->insert('tbl_booking', $bookingdata);
+		}
+		else 
+		{
+			$message = '';
+		}
+		redirect('/index.php/events/getEvents');
+	}
+
+	function get_booking($user_email)
+	{
+		$data['booking_list'] = $this->Bookingmodel->get_booking_list($user_email);
+		$this->load->view('template/header');
+		$this->load->view('booking_view', $data);
+		$this->load->view('template/footer');
+	}
+	
+	function delete_booking($id, $user_email)
+	{
+		$this->db->where('id', $id);
+		$this->db->delete('tbl_booking');
+		redirect('index.php/booking/get_booking/'.$user_email);
 	}
 }
 ?>
